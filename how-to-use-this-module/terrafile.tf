da648f9002a2f3406cb77fdf0100d4b08a835153
Ramon Borges <ramonboorges@gmail.com>
2022-02-10 20:59:02 -0300

    feat: Primeira versão do módulo de groundwork
    O módulo cria um compartment, e dentro dele cria uma VCN, uma Subnet, um internet gateway e a rota para utilizá-lo
    
    Ref: #1
    
    Revisado Por:
    Co-authored-by: Luiz Aoqui <lgfa29@gmail.com>
    Co-authored-by: Felipe Nobrega <lipenodias@gmail.com>
    Co-authored-by: Ramon Borges <ramonborges@gmail.com>
    Co-authored-by: Vinicius Cursi <viniciuscursi@gmail.com>
    Co-authored-by: Antonio Fernandes <afernandescneto@gmail.com>
    Co-authored-by: Selton Araujo <selton.araujo@escolar.ifrn.edu.br>

--- how-to-use-this-module/terrafile.tf
+++ how-to-use-this-module/terrafile.tf
@@ -1,16 +1,27 @@
-provider "hashicups" {
-  username = "mentoriaiac"
-  password = "2021@mentoria"
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
-module "hashicups_order" {
-  source       = "../"
-  order = {
-    Terraspresso = 4,
-    Nomadicano = 10
-    }  
+module "groundwork" {
+  source                        = "../"
+  root_compartment_id           = "ocid1.tenancy.oc1..aaaaaaaa7zimmsthvpvbveb6f6jkwvrny2sarj4sca2xawtqrjfsswjnyjfa"
+  compartment_name              = "MentoriaIaC"
+  compartment_description       = "Descrição - Mentoria IaC"
+  vcn_display_name              = "MentoriaIaC"
+  vcn_dns_label                 = "MentoriaIaC"
+  internet_gateway_display_name = "MentoriaIaC-igw"
+  subnet_display_name           = "MentoriaIaC"
+  subnet_dns_label              = "MentoriaIaC"
 }
 
-output "hashicups_order_id" {
-  value = module.hashicups_order.order_id
+output "vcn_cidr" {
+  value = module.groundwork.vcn_cidr
+}
+
+output "subnet_cidr" {
+  value = module.groundwork.subnet_cidr
 }
---
+++ how-to-use-this-module/variables.tf
@@ -0,0 +1,6 @@
+## Provider Configuration ##
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "private_key_path" {}
+variable "fingerprint" {}
+variable "region" {}
